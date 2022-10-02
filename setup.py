8b82fae971a07584a6ea873ab0ea213dc586c84d
Marc Mast <marc.mast@topicus.nl>
2022-10-02 15:51:38 +0200

    Added dot Co-authored-by: Marckck <connect.with.marck@gmail.com>

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ Basic usage:
         ]
     }
     post_vault_record = keyhub_vault.post_vault_record(group_uuid=settings['vault_uuid'], payload=payload)
+    
 ```
 
 > This repository is a cherry pick from the original authors [repo](https://github.com/topicusonderwijs/keyhub-sdk) and modified to fit some more specific needs
